e072da9a7a4a6039bbc99a1f0b70c0c4a5eef549
renzJaskin <jaskin.agmata@gmail.com>
2024-10-17 22:27:43 +0800

    feat: added lazy loading to routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
 // App.tsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './features/layout/Layout';
-import HomePage from './pages/HomePage';
-import ContactPage from './pages/ContactPage';
-import NotFoundPage from './pages/NotFoundPage';
+
+import { Suspense, lazy } from 'react';
+
+// Funtion test to intentionaly delay lazy load
+const delayImport = <T,>(
+  importFunc: () => Promise<T>,
+  delay: number = 2000
+): Promise<T> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(importFunc());
+    }, delay);
+  });
+};
+
+// Lazy-loaded components
+const HomePage = lazy(() => delayImport(() => import('./pages/HomePage')));
+const ContactPage = lazy(() => delayImport(() => import('./pages/ContactPage')));
+const NotFoundPage = lazy(() => delayImport(() => import('./pages/NotFoundPage')));
+
+// const HomePage = lazy(() => import('./pages/HomePage'));
+// const ContactPage = lazy(() => import('./pages/ContactPage'));
+// const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path='contact' element={<ContactPage />} />
-          <Route path='*' element={<NotFoundPage />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path='contact' element={<ContactPage />} />
+            <Route path='*' element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
